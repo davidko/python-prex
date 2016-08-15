17daf18497436ab20dd1d4b61b3c43ea134aaed8
David Ko <david@barobo.com>
2016-08-15 09:26:45 -0700

    Bumped to version 0.0.7

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ README = codecs.open(os.path.join(here, 'README.txt'), encoding='utf8').read()
 setup (name = 'prex',
        author = 'David Ko',
        author_email = 'david@barobo.com',
-       version = '0.0.6',
+       version = '0.0.7',
        description = "Execute Python scripts on a remote sandbox",
        long_description = README,
        package_dir = {'':'src'},
